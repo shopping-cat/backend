7b13b74a3e536a9cea45d55744d15ef6f5b4b11e
hyun <coderhyun476@gmail.com>
2021-03-30 23:57:32 +0900

    order complane

--- src/resolvers/seller/order.ts
+++ src/resolvers/seller/order.ts
@@ -4,7 +4,6 @@ import addPoint from "../../utils/addPoint";
 import bankNameToBankCode from "../../utils/bankNameToBankCode";
 import errorFormat from "../../utils/errorFormat";
 import getISeller from "../../utils/getISeller";
-import salePrice from "../../utils/salePrice";
 
 export const newOrders = queryField(t => t.list.field('newOrders', {
     type: 'Order',
@@ -138,7 +137,7 @@ export const exchangeRequestOrders = queryField(t => t.list.field('exchangeReque
         const orders = await ctx.prisma.order.findMany({
             where: {
                 item: { shopId: seller.shopId },
-                state: '환불처리',
+                state: '교환중',
                 payment: { state: '정상처리' },
             },
             orderBy: {
@@ -162,7 +161,7 @@ export const refundedOrders = queryField(t => t.list.field('refundedOrders', {
         const orders = await ctx.prisma.order.findMany({
             where: {
                 item: { shopId: seller.shopId },
-                state: '구매확정',
+                state: '환불처리',
                 payment: { state: '정상처리' },
             },
             orderBy: {
@@ -231,6 +230,7 @@ export const registDelivery = mutationField(t => t.field('registDelivery', {
     }
 }))
 
+// 주문 취소 금액환불
 export const cancelOrder = mutationField(t => t.field('cancelOrder', {
     type: 'Order',
     args: {
@@ -338,3 +338,126 @@ export const cancelOrder = mutationField(t => t.field('cancelOrder', {
         return order
     }
 }))
+
+
+export const refundOrder = mutationField(t => t.field('refundOrder', {
+    type: 'Order',
+    args: {
+        id: nonNull(intArg())
+    },
+    resolve: async (_, { id }, ctx) => {
+        const seller = await getISeller(ctx)
+        const prevOrder = await ctx.prisma.order.findUnique({
+            where: { id },
+            include: {
+                item: true,
+                coupons: true,
+                payment: {
+                    include: {
+                        orders: true
+                    }
+                },
+                user: {
+                    include: {
+                        refundBankAccount: true
+                    }
+                }
+            }
+        })
+        if (!prevOrder) throw errorFormat('존재하지 않는 주문입니다')
+        if (prevOrder.item.shopId !== seller.shopId) throw errorFormat('권한이 없습니다')
+        if (prevOrder.state !== '환불중') throw errorFormat(`${prevOrder.state} 상태에서는 환불이 불가능합니다`)
+
+        // 현금을 할 수 있는만큼 먼저 취소처리하고 나머지는 포인트 환불로처리
+        const cancelAblePrice = prevOrder.payment.totalPrice - prevOrder.payment.cancelPrice
+        const cancelAblePoint = prevOrder.payment.pointSale - prevOrder.payment.cancelPoint
+        const cancelPrice = cancelAblePrice >= prevOrder.totalPrice ? prevOrder.totalPrice : cancelAblePrice
+        const cancelPoint = cancelAblePrice >= prevOrder.totalPrice ? 0 : prevOrder.totalPrice - cancelAblePrice
+        if (cancelPoint > cancelAblePoint) throw errorFormat('포인트 환불 오류')
+        // 부분환불
+        if (cancelPrice > 0) {
+            const getToken = await axios.post(
+                'https://api.iamport.kr/users/getToken',
+                {
+                    imp_key: process.env.IAMPORT_REST_API_KEY,
+                    imp_secret: process.env.IAMPORT_REST_API_SECRET
+                }
+            )
+            if (!getToken?.data?.response) throw errorFormat('결제정보 조회 실패')
+            const { access_token } = getToken.data.response // 인증 토큰
+
+            const getCancelData = await axios.post(
+                'https://api.iamport.kr/payments/cancel',
+                {
+                    merchant_uid: prevOrder.payment.id,
+                    checksum: cancelAblePrice,
+                    amount: cancelPrice,
+                    refund_holder: prevOrder.payment.paymentMethod === '가상계좌' ? prevOrder.user.refundBankAccount?.ownerName : undefined,
+                    refund_bank: prevOrder.payment.paymentMethod === '가상계좌' && prevOrder.user.refundBankAccount ? bankNameToBankCode(prevOrder.user.refundBankAccount.bankName) : undefined,
+                    refund_account: prevOrder.payment.paymentMethod === '가상계좌' ? prevOrder.user.refundBankAccount?.accountNumber : undefined
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": access_token
+                    }
+                }
+            )
+            const { response } = getCancelData.data // 환불 결과
+            if (!response) throw errorFormat(getCancelData.data.message || '환불 오류')
+        }
+
+        // 포인트 환불
+        if (cancelPoint > 0) {
+            await addPoint(cancelPoint, '부분 환불', prevOrder.user.id, ctx)
+        }
+
+        // DB에 저장
+        await ctx.prisma.payment.update({
+            where: { id: prevOrder.paymentId },
+            data: {
+                cancelPrice: { increment: cancelPrice },
+                cancelPoint: { increment: cancelPoint }
+            }
+        })
+
+        // 쿠폰 환불
+        for (const coupon of prevOrder.coupons) {
+            try {
+                await ctx.prisma.coupon.update({
+                    where: { id: coupon.id },
+                    data: {
+                        order: { disconnect: true }
+                    }
+                })
+            } catch (error) {
+
+            }
+        }
+
+        const order = await ctx.prisma.order.update({
+            where: { id },
+            data: {
+                state: '환불처리'
+            }
+        })
+        return order
+    }
+}))
+
+export const exchangeOrder = mutationField('exchangeOrder', {
+    type: 'Order',
+    args: {
+        id: nonNull(intArg())
+    },
+    resolve: async (_, { id }, ctx) => {
+        // TODO push message
+        const order = ctx.prisma.order.update({
+            where: { id },
+            data: {
+                state: '교환처리'
+            }
+        })
+        return order
+    }
+})
