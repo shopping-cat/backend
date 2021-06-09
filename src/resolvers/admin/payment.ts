eb31852eba04243fa7c8f573bbc845286d9fee0d
hyun <coderhyun476@gmail.com>
2021-06-09 19:11:00 +0900

    paymentDetail

--- src/resolvers/admin/payment.ts
+++ src/resolvers/admin/payment.ts
@@ -1,10 +1,13 @@
-import { intArg, nonNull, queryField } from "nexus"
+import { intArg, nonNull, queryField, stringArg } from "nexus"
 
 export const payments = queryField(t => t.list.field('payments', {
     type: 'Payment',
     resolve: async (_, { }, ctx) => {
         const payments = await ctx.prisma.payment.findMany({
-            where: { state: { notIn: ['결제요청', '결제취소'] } }
+            where: { state: { notIn: ['결제요청', '결제취소'] } },
+            orderBy: {
+                id: 'desc'
+            }
         })
         return payments
     }
@@ -13,7 +16,7 @@ export const payments = queryField(t => t.list.field('payments', {
 export const payment = queryField(t => t.field('payment', {
     type: 'Payment',
     args: {
-        id: nonNull(intArg())
+        id: nonNull(stringArg())
     },
     resolve: async (_, { id }, ctx) => {
         const payment = await ctx.prisma.payment.findUnique({
